9191441e83bb1649277d37b615487d65cb351179
flag111 <496047736@qq.com>
2016-08-23 22:02:30 +0800

    Update list.php

--- message_board/list.php
+++ message_board/list.php
@@ -1,24 +1,26 @@
 <?php 
 include 'conn.php'; 
 ?> 
-<?php 
-echo "<div align='center'><a href='add.php'>继续添加</a></div>"; 
-?> 
-<table width=500 border="0" align="center" cellpadding="5" cellspacing="1" bgcolor="#add3ef"> 
+
+<div style="text-align:center"><a href="add.php">继续添加</a></div>
+ 
+<table style="width:500px; border:0px; align:center; background:#add3ef;" cellpadding="5" cellspacing="1"> 
 <?php 
 $sql="select * from message order by id"; 
 $query=mysql_query($sql); 
 while ($row=mysql_fetch_array($query)){ 
 ?> 
 
-<tr bgcolor="#eff3ff"> 
-<td>标题：<font color="red"><?=$row[title]?></font> 用户：<font color="red"><?=$row[user] ?></font><div align="right"><a href="preEdit.php?id=<?=$row[id]?>">编辑</a>  |  <a href="delete.php?id=<?=$row[id]?>">删除</a></div></td> 
+<tr style="background:#eff3ff"> 
+    <td>标题：<span style="color:red"><?=$row[title]?></span> 用户：<span style="color:red"><?=$row[user] ?></span>
+        <div style="text-align:right"><a href="preEdit.php?id=<?=$row[id]?>">编辑</a>  |  <a href="delete.php?id=<?=$row[id]?>">删除</a></div>
+    </td> 
 </tr> 
-<tr bgColor="#ffffff"> 
-<td>内容：<?=$row[content]?></td> 
+<tr style="background:#ffffff"> 
+    <td>内容：<?=$row[content]?></td> 
 </tr> 
-<tr bgColor="#ffffff"> 
-<td><div align="right">发表日期：<?=$row[lastdate]?></div></td> 
+<tr style="background:#ffffff"> 
+    <td><div style="text-align:right">发表日期：<?=$row[lastdate]?></div></td> 
 </tr> 
 <?php }?> 
 </table> 
